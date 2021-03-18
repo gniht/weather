3e5e67e2b18ee3d0c9d85b561b20af2deae69334
Jason Thomas <jasonthomasjat@gmail.com>
2021-03-18 00:14:48 -0700

    censoring apiKey

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// apiKey 7a839f13d0e3b2951b240ec2a8ae6302
+apiKey = '';
 
 /* https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} */
 
 // Portland, OR 45.5051 N, 122.6750 W
 
-async function localWeather(latitude, longitude){
+async function localWeather(latitude, longitude, apiKey){
   //hit the api and grab the data for the given coordinates
-  let resolve = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=7a839f13d0e3b2951b240ec2a8ae6302`, 
+  let resolve = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}`, 
   { mode: 'cors' });
   let data = await resolve.json();
   console.log(data); 
